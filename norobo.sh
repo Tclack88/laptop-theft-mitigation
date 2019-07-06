0350743aaa08a2914c86594a1fd6caf2c118305b
Tclack88 <39845330+Tclack88@users.noreply.github.com>
2019-07-06 11:21:16 -0700

    update norobo.sh
    Used to still run scripts when non-guest was logged into network

--- norobo.sh
+++ norobo.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-network.sh; wc.sh; geolocation.py; keylog.py; sleep 29m && ping -c 1 google.com > /dev/null
-STATUS=$?
-if [[ $STATUS == 0 ]]; then mailout.sh; fi
+if [ $(who | awk '{print $1}') == guest ]
+then
+        network.sh
+        wc.sh
+        geolocation.py
+        keylog.py
+        sleep 28m && ping -c 1 google.com > /dev/null
+        STATUS=$?
+        if [[ $STATUS == 0 ]]; then mailout.sh; fi
+fi
